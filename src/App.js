00121c4fc74eea29d3ba1f2d7493cd7958ce8ed0
Moise Uwimana <moiseuwimana04@gmail.com>
2023-10-09 09:25:58 +0200

    End game part 2

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import Die from './components/Die'
 
 export default function App(){
   /**
-   * Challenge: 
-   * 1. Add new state called 'tenzies', default to false. It represents whether the user has won the game yet or not.
-   * 2. Add an effect that runs every time the 'dice' state array changes. For now, just console.log("Dice state changed")
+   * Challenge: Check the dice array for these winning conditions:
+   * 1. All dice are held, and
+   * 2. All dice have the same value
+   * 
+   * If both conditions are true, set 'tenzies' to true and log "You won!" to the console
    */
 
   const [dice, setDice] = React.useState(allNewDice())
@@ -19,7 +21,12 @@ export default function App(){
 
 
   React.useEffect(()=>{
-    console.log("Dice state changed")
+    const allHeld = dice.every(die => die.isHeld);
+    const allSameValue = dice.every(die => die.value === dice[0].value)
+    if (allHeld && allSameValue){
+      setTenzies(true);
+      console.log("You won!!!")
+    }
   },[dice])
 
 
